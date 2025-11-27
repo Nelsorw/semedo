4a3824b09b8261e72d2c2ab91b00d04d503b7355
Nelson TUYISHIMIRE <nelson02122002nelson@gmail.com>
2025-11-27 20:49:14 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! This is my Node.js web app.');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
